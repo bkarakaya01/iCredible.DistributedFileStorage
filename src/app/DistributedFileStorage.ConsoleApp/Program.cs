0bb4ef8a778d831b846d96ce6118671a66b91562
bkarakaya01 <bkarakaya01@gmail.com>
2025-07-27 15:31:58 +0300

    core: setup ioc on console app, adds configuration builder & adds sample example

--- src/app/DistributedFileStorage.ConsoleApp/Program.cs
+++ src/app/DistributedFileStorage.ConsoleApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DistributedFileStorage.Application.Services;
+using DistributedFileStorage.IoC.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json");
+
+var configuration = builder.Build();
+
+var services = new ServiceCollection();
+services.AddDistributedFileStorage(configuration);
+
+var provider = services.BuildServiceProvider();
+
+var chunkService = provider.GetRequiredService<ChunkService>();
+var reconstructor = provider.GetRequiredService<Reconstructor>();
+
+string filePath = Path.Combine("Files", "sample.pdf");
+string outputPath = Path.Combine("Files", "reconstructed.pdf");
+
+Console.WriteLine("-> Chunking...");
+var chunks = await chunkService.ChunkAndStoreAsync(filePath);
+
+Console.WriteLine("-> Reconstructing...");
+await reconstructor.ReconstructFileAsync(chunks.First().FileMetadataId, outputPath);
